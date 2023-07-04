f3ad2ebd1397437fc3cfe41223f720e5bf7c2455
shovonSharma <sharmashovon875@gmail.com>
2023-07-04 18:02:38 +0600

    commit 1

--- app/src/main/java/com/example/cardiacrecorder/MainActivity.java
+++ app/src/main/java/com/example/cardiacrecorder/MainActivity.java
@@ -4,22 +4,60 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+/**
+ * Showing Splash Screen
+ */
 public class MainActivity extends AppCompatActivity {
-    Button button;
+
+    private static int SPLASH_SCRN=4000;
+
+    Animation topAnim,bottomAnim;
+
+    ImageView imageView;
+    TextView logo ,slogan;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        button=(Button) findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
+
+        //animation
+        topAnim= AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        bottomAnim= AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
+
+
+        //hocks
+
+        imageView=findViewById(R.id.splashScreenImg);
+        logo=findViewById(R.id.textView2);
+        slogan=findViewById(R.id.textView5);
+
+        imageView.setAnimation(topAnim);
+        logo.setAnimation(bottomAnim);
+        slogan.setAnimation(bottomAnim);
+
+        /**
+         * splash screen dynamic
+         */
+        new Handler().postDelayed(new Runnable() {
             @Override
-            public void onClick(View v) {
-                Intent intent=new Intent(MainActivity.this,LoginActivity.class);
+            public void run() {
+
+                Intent intent=new Intent(MainActivity.this,Home.class);
                 startActivity(intent);
+                finish();
+
             }
-        });
+        },SPLASH_SCRN);
+
+
+
+
     }
 }
