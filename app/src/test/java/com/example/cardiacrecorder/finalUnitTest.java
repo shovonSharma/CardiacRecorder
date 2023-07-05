721d28c440d240587a89b65c1e81823e6478aba1
shovonSharma <sharmashovon875@gmail.com>
2023-07-05 17:21:28 +0600

    still error

--- app/src/test/java/com/example/cardiacrecorder/finalUnitTest.java
+++ app/src/test/java/com/example/cardiacrecorder/finalUnitTest.java
@@ -6,9 +6,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.RuntimeEnvironment;
-/**
- * data update test
- */
+
 @RunWith(RobolectricTestRunner.class)
 
 public class finalUnitTest {
--- app/src/test/java/com/example/cardiacrecorder/testDeleteUnitTest.java
+++ app/src/test/java/com/example/cardiacrecorder/testDeleteUnitTest.java
@@ -6,9 +6,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.RuntimeEnvironment;
-/**
- * data deletion test
- */
+
 @RunWith(RobolectricTestRunner.class)
 
 
